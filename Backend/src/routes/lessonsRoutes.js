5b982d2d3ba0a51235148b99d8c919180ef1c177
Kauan Ramos <kauanramos03@outlook.com>
2024-03-11 10:50:46 -0300

    Add courses

--- Backend/src/routes/lessonsRoutes.js
+++ Backend/src/routes/lessonsRoutes.js
@@ -4,8 +4,8 @@ const LessonsController = require('../controllers/lessonsController');
 const router = Router();
 const lessonsController = new LessonsController();
 
-router.post('/courses/lessons', (req, res) => { lessonsController.Create(req, res); });
-router.delete('/courses/:courseid/lessons/:id', (req, res) => { lessonsController.Delete(req, res); });
+// router.post('/courses/lessons', (req, res) => { lessonsController.Create(req, res); });
+// router.delete('/courses/:courseid/lessons/:id', (req, res) => { lessonsController.Delete(req, res); });
 router.get('/courses/:courseid/lessons', (req, res) => { lessonsController.ReadById(req, res); });
 
 module.exports = router;
